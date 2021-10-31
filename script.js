b0eaebce033b1d84693a9d365516785cd69f7e99
saywordsahn <saywordsahn@gmail.com>
2021-10-31 16:02:21 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const timeleft = document.querySelector('#timeLeft');
+const score = document.querySelector('#score');
+const squares = document.querySelectorAll('.square');
+
+console.log(squares);
